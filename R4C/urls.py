384fb5882049c62db62c0c630e60bae638333011
Ruslan Yusupov <rbyusupov@gmail.com>
2023-09-29 12:54:59 +0300

    robot/ API endpoint

--- R4C/urls.py
+++ R4C/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from robots.views import robot
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("robot/", robot),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from robots.models import Robot
+from robots.utils import save_robot_if_clean_data
+
+
+@csrf_exempt
+def robot(request):
+    if request.method == "GET":
+        # Prepare and send data for all instances Robot
+        data = serializers.serialize("json", Robot.objects.all())
+
+        return JsonResponse(json.loads(data), safe=False)
+
+    if request.method == "POST":
+        # Convert request data into dict
+        request_data = json.loads(request.body.decode("utf-8"))
+        # Save the data if it is valid
+        response_data = save_robot_if_clean_data(request_data)
+
+        return JsonResponse(data=response_data["data"], status=response_data["status"])
